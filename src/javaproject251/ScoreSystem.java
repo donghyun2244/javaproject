3f70e047c19d3262165286146d4c32898d52a718
NSU <NSU@192.192.34.45>
2025-11-10 11:43:05 +0900

    main 수정

--- src/javaproject251/ScoreSystem.java
+++ src/javaproject251/ScoreSystem.java
@@ -1,10 +1,42 @@
 package javaproject251;
 
+import java.util.Scanner;
+
 public class ScoreSystem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		loginMenu();
+		
 
 	}
-
+	
+	public static void loginMenu() {
+		System.out.println("----------------------------------------------");
+		System.out.println("로그인");
+		System.out.println("학생이면 1 교수면 2를 입력");
+		
+		Scanner scanner = new Scanner(System.in); 
+		int a=scanner.nextInt();
+		if(a==1) {
+			//학생 기능 실행
+		}else if(a==2) {
+			//교수기능 실행
+		}else {
+			//잘못된 숫자를 입력하셨습니다. 다시 입력해주세요.
+		}
+		
+	}
+	
+	public static void loginStudent() {
+		
+	}
+	
+	public static void loginpr() {
+		
+	}
 }
+
+
+
+
+
